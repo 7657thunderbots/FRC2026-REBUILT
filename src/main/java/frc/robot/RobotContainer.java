f9eb7be0a6ce2a88b0905955c6670482d979fb9b
Xavier Schrage <schragex@gmail.com>
2026-02-28 17:21:47 -0600

    Progress has been made.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,38 +12,24 @@ import edu.wpi.first.wpilibj2.command.*;
 import edu.wpi.first.wpilibj2.command.button.*;
 import edu.wpi.first.wpilibj.Filesystem;
 
-/**
- * This class is where the bulk of the robot should be declared. Since
- * Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in
- * the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of
- * the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-
-  private final SwerveSubsystem drivebase = new SwerveSubsystem(
+  private final SwerveSubsystem m_swerveSubsystem = new SwerveSubsystem(
       new File(Filesystem.getDeployDirectory(), "swerve/neo"));
 
-  // private final Vision m_Vision = new
-  // Vision(drivebase.getSwerveDrive()::addVisionMeasurement, drivebase);
-
-  // private final VisionSimSubSystem m_SimSubSystem = new
-  // VisionSimSubSystem(m_Vision);
+  private final VisionSubsystem m_visionSubsystem = new VisionSubsystem(() -> m_swerveSubsystem.getPose(),
+      m_swerveSubsystem.getSwerveDrive().field, m_swerveSubsystem.getSwerveDrive()::addVisionMeasurement);
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController = new CommandXboxController(
       OperatorConstants.kDriverControllerPort);
 
-  SwerveInputStream driveAngularVelocity = SwerveInputStream.of(drivebase.getSwerveDrive(),
+  SwerveInputStream driveAngularVelocity = SwerveInputStream.of(
+      m_swerveSubsystem.getSwerveDrive(),
       () -> m_driverController.getLeftY() * -1,
       () -> m_driverController.getLeftX() * -1)
       .withControllerRotationAxis(m_driverController::getRightX)
       .deadband(OperatorConstants.DEADBAND)
-      .scaleTranslation(0.8)
+      // .scaleTranslation(0.8)
       .allianceRelativeControl(true)
       .scaleRotation(-1.0);
 
@@ -53,42 +39,20 @@ public class RobotContainer {
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
-    // m_driverController.button(3).onTrue(drivebase.sysIdDriveMotorCommand());
-    // m_driverController.button(4).onTrue(drivebase.sysIdAngleMotorCommand());
   }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be
-   * created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
-   * an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
-   * {@link
-   * CommandXboxController
-   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or
-   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition).onTrue(new ExampleCommand(m_exampleSubsystem));
+    Command driveFieldOrientedAnglularVelocity = m_swerveSubsystem.driveFieldOriented(driveAngularVelocity);
 
-    Command driveFieldOrientedAnglularVelocity = drivebase.driveFieldOriented(driveAngularVelocity);
+    m_swerveSubsystem.zeroGyroWithAlliance();
 
-    drivebase.zeroGyroWithAlliance();
+    m_swerveSubsystem.setDefaultCommand(driveFieldOrientedAnglularVelocity);
 
-    drivebase.setDefaultCommand(driveFieldOrientedAnglularVelocity);
-  }
+    m_driverController.a().whileTrue(m_swerveSubsystem.driveForward());
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    m_driverController.povUp().whileTrue(m_swerveSubsystem.PointWheelsAt(0));
+    m_driverController.povRight().whileTrue(m_swerveSubsystem.PointWheelsAt(90));
+    m_driverController.povDown().whileTrue(m_swerveSubsystem.PointWheelsAt(180));
+    m_driverController.povLeft().whileTrue(m_swerveSubsystem.PointWheelsAt(270));
   }
 }
