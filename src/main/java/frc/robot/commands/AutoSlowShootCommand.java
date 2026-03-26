597e55622f8bc04bc0a0758b24b80046bf98c8f6
Gavin Mitchell <gavin.mitchell@ntithunderbots.com>
2026-03-26 15:24:19 -0500

    Made shoot command for auto so preload auto still works

--- src/main/java/frc/robot/commands/AutoSlowShootCommand.java
+++ src/main/java/frc/robot/commands/AutoSlowShootCommand.java
@@ -2,15 +2,19 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.SpindexerSubsystem;
 import static frc.robot.Constants.*;
 
-public class SlowShootCommand extends Command {
+public class AutoSlowShootCommand extends Command {
+    private final ShooterSubsystem m_ShooterSubsystem;
     private final SpindexerSubsystem m_SpindexerSubsystem;
     private final Timer motorStartup = new Timer();
 
-    public SlowShootCommand(SpindexerSubsystem spindexer) {
+    public AutoSlowShootCommand(ShooterSubsystem shooter, SpindexerSubsystem spindexer) {
+        m_ShooterSubsystem = shooter;
         m_SpindexerSubsystem = spindexer;
+        addRequirements(shooter);
         addRequirements(spindexer);
     }
     // set shoot velocity 5000
@@ -18,11 +22,13 @@ public class SlowShootCommand extends Command {
     // set spindexer velocity 800
 
     public void initialize() {
+        m_ShooterSubsystem.setShootVelocity(ShooterConstants.SHOOT_RPMS);
         motorStartup.restart();
 
     }
 
     public void execute() {
+        m_ShooterSubsystem.setShootVelocity(ShooterConstants.SHOOT_RPMS);
         if (motorStartup.hasElapsed(1)) {
             m_SpindexerSubsystem.setKickerVelocity(SpindexerConstants.KICKER_SPEED);
             m_SpindexerSubsystem.setSpindexerVelocity(SpindexerConstants.SPINDEXER_SPEED);
@@ -33,6 +39,7 @@ public class SlowShootCommand extends Command {
     public void end(boolean interrupted) {
         m_SpindexerSubsystem.setSpindexerVelocity(0);
         m_SpindexerSubsystem.setKickerVelocity(0);
+        m_ShooterSubsystem.setShootVelocity(0);
     }
 
     // public boolean isFinished() {
